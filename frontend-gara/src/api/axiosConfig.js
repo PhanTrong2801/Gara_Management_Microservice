717f72056acbf65d634d76ee8e890f569295b870
Trong <phanthanhtrong66@gmail.com>
2026-07-30 20:18:22 +0700

    add firebase

--- frontend-gara/src/api/axiosConfig.js
+++ frontend-gara/src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
-    // baseURL: "http://api.gara-autoflow.online:8080/api",
+    // baseURL: "http://localhost:8080/api",
+    baseURL: "http://api.gara-autoflow.online:8080/api",
 });
 
 api.interceptors.request.use((config) => {
