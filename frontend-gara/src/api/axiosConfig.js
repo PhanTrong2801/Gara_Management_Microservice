243e407d2bc3cdb77b7d6653fa866fc559e64ceb
Trong <phanthanhtrong66@gmail.com>
2026-08-15 20:42:57 +0700

    deploy frontendfrontend

--- frontend-gara/src/api/axiosConfig.js
+++ frontend-gara/src/api/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    // baseURL: "http://localhost:8080/api",
     // baseURL: "http://api.gara-autoflow.online:8080/api",
+    baseURL: "https://api.gara-autoflow.online/api",
 });
 
 api.interceptors.request.use((config) => {
